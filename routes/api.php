b90bcfd4f3c04d643e06f104201265f2d18a3247
msirhy <msirhy@gmail.com>
2022-01-19 14:15:33 +0200

    Seeding fake data with a factory

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Person;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/person',function(){
-$person=[
-    'f_name'=>'Ahmed',
-    'l_name'=>'Khaled'
-];
-
+Route::get('/person/{person}',function(Person $person){
     return $person;
 });
